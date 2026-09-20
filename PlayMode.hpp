8d5894712cf19a6dbd7bb3a9d67562a62a7f1310
ErwinZhou <erwinzhou@cmu.edu>
2026-09-20 14:53:54 -0400

    Add game main loop for gameMode

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -1,51 +1,23 @@
+#pragma once
 #include "Mode.hpp"
-
-#include "Scene.hpp"
-#include "Sound.hpp"
-
-#include <glm/glm.hpp>
-
-#include <vector>
-#include <deque>
+#include "Story.hpp"
+#include "FontAsset.hpp"
 
 struct PlayMode : Mode {
-	PlayMode();
-	virtual ~PlayMode();
+    PlayMode();
+    ~PlayMode() override = default;
 
-	//functions called by main loop:
-	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
-	virtual void update(float elapsed) override;
-	virtual void draw(glm::uvec2 const &drawable_size) override;
+    bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
+    void update(float elapsed) override;
+    void draw(glm::uvec2 const &drawable_size) override;
 
-	//----- game state -----
+    void enter_node(uint32_t id);
+    void select_choice(uint32_t option);
 
-	//input tracking:
-	struct Button {
-		uint8_t downs = 0;
-		uint8_t pressed = 0;
-	} left, right, down, up;
-
-	//local copy of the game scene (so code can change it during gameplay):
-	Scene scene;
-
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
-
-	glm::vec3 get_leg_tip_position();
-
-	//music coming from the tip of the leg (as a demonstration):
-	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
-
-	//car honk sound:
-	std::shared_ptr< Sound::PlayingSample > honk_oneshot;
-	
-	//camera:
-	Scene::Camera *camera = nullptr;
-
+    Story story;
+    FontAsset font;
+    uint32_t current_node = 0;
+    uint32_t selected_choice = 0;
+    float scroll_y = 0.0f;
+    bool layout_dirty = true;
 };
